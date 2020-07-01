54ac9201ef54ad622629e35faf389ca5fcc88dba
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2020-07-01 15:38:13 -0300

    Adiciona endpoint de find by id de Usuario

--- Protect/src/main/java/com/project/Protect/controller/UsuarioController.java
+++ Protect/src/main/java/com/project/Protect/controller/UsuarioController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +33,12 @@ public class UsuarioController {
 	public ResponseEntity<List<Usuario>> getUsuarios() {
 		List<Usuario> lista = usuarioService.getUsuarios();
 		return ResponseEntity.ok().body(lista);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> findById(@PathVariable Integer id) {
+		Usuario usuario = usuarioService.findById(id);
+		return ResponseEntity.ok().body(usuario);
 	}
 
 }
