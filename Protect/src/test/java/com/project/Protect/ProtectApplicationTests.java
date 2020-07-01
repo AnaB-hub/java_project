e9046220029665c20e75c8580b98ec70cceaf83a
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2020-07-01 14:09:53 -0300

    Adiciona projeto inicial, utilizando java, spring, lombok e postgresql

---
+++ Protect/src/test/java/com/project/Protect/ProtectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Protect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
