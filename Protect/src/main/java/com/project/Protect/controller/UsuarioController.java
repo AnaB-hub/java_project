fe5043ee80dce0691bfe86be0188e070898cbfd6
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2020-07-01 16:03:31 -0300

    Ajustes no nome do métodp de buscar ativos

--- Protect/src/main/java/com/project/Protect/controller/UsuarioController.java
+++ Protect/src/main/java/com/project/Protect/controller/UsuarioController.java
@@ -51,7 +51,7 @@ public class UsuarioController {
 	}
 	
 	@GetMapping("/ativos")
-	public Stream<Usuario> produtosAtivos() {
+	public Stream<Usuario> ativos() {
 		return usuarioService.findAtivos();
 	}
 
